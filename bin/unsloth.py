0647b7387585168ff3367207fe2955f593b20f04
Mattia Braga <mattiabraga2006@gmail.com>
2025-03-12 22:04:12 +0100

    program mods

--- bin/unsloth.py
+++ bin/unsloth.py
@@ -62,7 +62,7 @@ Solve the given math problem step by step. Show your reasoning and provide the f
 dataset = load_dataset("open-r1/OpenR1-Math-220k", "all", split = "train[0:500]",trust_remote_code=True)
 print(dataset)
 print(dataset[1])
-print(dataset[1]["generations"][0])
+# print(dataset[1]["generations"][0])
 
 prompt_style = """Below is an instruction that describes a task, paired with an input that provides further context. 
 Write a response that appropriately completes the request. 
@@ -98,7 +98,7 @@ def formatting_prompts_func(examples):
     }
 
 dataset_finetune = dataset.map(formatting_prompts_func, batched = True)
-dataset_finetune["text"][0]
+print(dataset_finetune["text"][0])
 
 model_lora = FastLanguageModel.get_peft_model(
     model,
@@ -169,16 +169,6 @@ print(response[0].split("### Response:")[1])
 
 trainer_stats = trainer.train()
 
-try:
-    model.save_pretrained_merged("DeepSeek-R1-Distill-Llama-8B-OpenR1-Math", tokenizer, save_method="merged_16bit")
-    model.push_to_hub_merged("Neetree/DeepSeek-R1-Distill-Llama-8B-OpenR1-Math", tokenizer, save_method="merged_16bit", token=hugging_face_token)
-except Exception as e:
-    print(f"Model saving failed with error: {e}")
-    wandb.log({"Model Saving Error": str(e)})
-    raise e
-
-wandb.finish()
-
 question = """Unified State Exam in Anchuria. (7-11 grades, 2 points). In Anchuria, the Unified State Exam is taking place.
 The probability of guessing the correct answer to each question on the exam is 0.25. In 2011, to receive a certificate, one needed to answer correctly 3 questions out of 20.
 In 2012, the School Administration of Anchuria decided that 3 questions were too few. Now, one needs to answer correctly 6 questions out of 40.
@@ -196,3 +186,14 @@ outputs = model_lora.generate(
 response = tokenizer.batch_decode(outputs)
 
 print(response[0].split("### Response:")[1])
+
+
+try:
+    model.save_pretrained_merged("DeepSeek-R1-Distill-Llama-8B-OpenR1-Math", tokenizer, save_method="merged_16bit")
+    model.push_to_hub_merged("Neetree/DeepSeek-R1-Distill-Llama-8B-OpenR1-Math", tokenizer, save_method="merged_16bit", token=hugging_face_token)
+except Exception as e:
+    print(f"Model saving failed with error: {e}")
+    wandb.log({"Model Saving Error": str(e)})
+    raise e
+
+wandb.finish()
